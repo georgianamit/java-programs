c6f9ad567e0a1f530965dd7323ad0f28e4582ee1
Amit Rawat <georgianamitrawat@gmail.com>
2019-05-28 22:54:55 +0530

    hello worl program

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello" + " " + "World");
+        System.out.println("Hello\nWorld");
+        System.out.println("Hello");
+        System.out.println("World");
+
+    }
+}
